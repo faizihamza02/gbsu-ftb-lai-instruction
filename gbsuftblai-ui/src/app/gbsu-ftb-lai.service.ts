45b1969e06786477b845f14e98817144d98d07b2
Faizi <Hamza>
2022-02-28 18:11:40 +0100

    add service convertNumber

--- gbsuftblai-ui/src/app/gbsu-ftb-lai.service.ts
+++ gbsuftblai-ui/src/app/gbsu-ftb-lai.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Result } from './model/result';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GbsuFtbLaiService {
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { }
+
+  convertNumber(number:number){
+    return this._httpClient.get<Result>("http://localhost:8080//gbsu-ftb-lai/"+number);
+  }
 
 }
