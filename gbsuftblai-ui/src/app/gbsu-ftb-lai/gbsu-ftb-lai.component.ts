323a511c356a1c262dd41b38e16aa50d43fec1f5
Faizi <Hamza>
2022-02-28 18:13:21 +0100

    call convert service + display results

--- gbsuftblai-ui/src/app/gbsu-ftb-lai/gbsu-ftb-lai.component.ts
+++ gbsuftblai-ui/src/app/gbsu-ftb-lai/gbsu-ftb-lai.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { GbsuFtbLaiService } from '../gbsu-ftb-lai.service';
+import { Result } from '../model/result';
 
 @Component({
   selector: 'app-gbsu-ftb-lai',
@@ -10,6 +11,8 @@ export class GbsuFtbLaiComponent implements OnInit, OnDestroy {
 
   constructor(private gbsuFtbLaiService: GbsuFtbLaiService) { }
 
+  listAlreadyResults : Array<NumberConverted> =[];
+
   ngOnInit(): void {
   }
 
@@ -17,6 +20,15 @@ export class GbsuFtbLaiComponent implements OnInit, OnDestroy {
   }
 
   convertNumber(inputNumber: number): void {
+    console.log(inputNumber);
+    this.gbsuFtbLaiService.convertNumber(inputNumber).subscribe(data => {
+       const numberConverted = {
+        numberToConvert : inputNumber,
+        result:"and the result is " + data.result
+       }
+       this.listAlreadyResults.push(numberConverted);
+       console.log(this.listAlreadyResults);
+    });
   }
 
 }
